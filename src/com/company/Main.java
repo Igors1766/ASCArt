b8f8f9a2575784ee0fb7b69f4f7f20342a4c1610
Java Courses 1 <JavaCourses1@tti.lv>
2020-02-06 19:41:33 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Number width");// write your code here
+        int width = scan.nextInt();
+        for (int i = 0; i < width; i++) {
+            System.out.print("*");
+        }
+
+	// write your code here
+    }
+}
+
